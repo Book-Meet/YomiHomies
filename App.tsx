da29ec54882781900bd4f975766b69014f705e9d
BBennettChiba <BrysonCBennett@gmail.com>
2021-04-13 17:45:56 +0900

    wrote authorization

--- App.tsx
+++ App.tsx
@@ -1,27 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
-
-import Amplify from 'aws-amplify'
+import Amplify, { API, graphqlOperation } from 'aws-amplify'
 import config from './src/aws-exports'
+import { listTodos } from './src/graphql/queries';
+import { Auth } from "@aws-amplify/auth";
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+
+
 Amplify.configure(config)
 
-export default function App() {
+function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+
+  (async function () {
+    const todos = await API.graphql(graphqlOperation(listTodos));
+    console.log('todos are :', todos);
+  })()
 
   if (!isLoadingComplete) {
     return null;
   } else {
     return (
+      <>
+      {/* <div> */}
+        {/* <AmplifySignOut />
+        My App
+        </div> */}
       <SafeAreaProvider>
         <Navigation colorScheme={colorScheme} />
         <StatusBar />
       </SafeAreaProvider>
+      </>
     );
   }
 }
+
+export default withAuthenticator(App);
