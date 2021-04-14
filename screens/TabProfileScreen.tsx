1b4d8a83c12dd3237331b45157923f8e1d9ccf85
Misako Kondoh <kondoh.misako@gmail.com>
2021-04-14 15:54:19 +0900

    Implementation of profile skelton

--- screens/TabProfileScreen.tsx
+++ screens/TabProfileScreen.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Image } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
@@ -8,6 +8,36 @@ export default function TabProfileScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Profile</Text>
+      <Image style={{
+            width: 100,
+            height: 100
+          }} source={require('../assets/images/arina-reading.jpeg')} />
+      <div>Username: Arina</div>
+      <div>Nickname: Ari</div>
+      <div>Gender: Female</div>
+      <div>Top Books: 
+        <div>Book1</div>
+        <div>Book2</div>
+        <input type="text"></input>
+        <button>+</button>
+      </div>
+      <div>Top Genres: 
+        <select>
+          <option value="" selected></option>
+          <option value="Thrillers">Thrillers</option>
+          <option value="Business">Business</option>
+          <option value="Romance">Romance</option>
+        </select>
+      </div>
+      <div>Top Authors: 
+        <div>Author1</div>
+        <div>Author2</div>
+        <input type="text"></input>
+        <button>+</button>
+      </div>
+      <div>About me: 
+        <div>Hello!!</div>
+      </div>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
     </View>
