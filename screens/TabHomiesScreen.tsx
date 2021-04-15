9e6c16ca1eab0ffc936c048f0b52dee94751961b
MattB09 <mattbancroft03@gmail.com>
2021-04-15 17:50:05 +0900

    Added Homies Screen with hardcoded data.

--- screens/TabHomiesScreen.tsx
+++ screens/TabHomiesScreen.tsx
@@ -1,16 +1,105 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, FlatList, SafeAreaView, Text, View } from 'react-native';
+import Navigation from '../navigation';
 
 import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+import { navItem } from '@aws-amplify/ui';
+
+interface matchItem {
+  id: Number;
+  nickname: String;
+  chatPreview: String;
+  newMessages: Number;
+}
+
+const DATA: matchItem[] = [
+  {
+    id: 1,
+    nickname: "GuyWithFace",
+    chatPreview: "I have a face! I like Books!",
+    newMessages: 0
+  },
+  {
+    id: 2,
+    nickname: "GirlWithFace",
+    chatPreview: "I have a face too! I like Books!",
+    newMessages: 0
+  },
+  {
+    id: 3,
+    nickname: "GirlWithBook",
+    chatPreview: "I have a book! I like Faces!",
+    newMessages: 2
+  },
+  {
+    id: 4,
+    nickname: "GirlWithFace",
+    chatPreview: "I have a face! I like Books!",
+    newMessages: 1
+  },
+  {
+    id: 5,
+    nickname: "DogWithFace",
+    chatPreview: "I'm a dog with a face'! I like eating Books!",
+    newMessages: 0
+  },
+  {
+    id: 6,
+    nickname: "DogWithBook",
+    chatPreview: "I'm a dog with a book I used to have a book but I ate it...! I like eating Books!",
+    newMessages: 0
+  },
+  {
+    id: 7,
+    nickname: "Booksy Williams",
+    chatPreview: "I'm a dog with a book I used to have a book but I ate it...! I like eating Books!",
+    newMessages: 0
+  },
+  {
+    id: 8,
+    nickname: "The Yomiest of all Homies",
+    chatPreview: "I'm a dog with a book I used to have a book but I ate it...! I like eating Books!",
+    newMessages: 0
+  },
+  {
+    id: 9,
+    nickname: "You've never Yomied like me",
+    chatPreview: "I'm a dog with a book I used to have a book but I ate it...! I like eating Books!",
+    newMessages: 0
+  },
+  {
+    id: 10,
+    nickname: "Daddy's Home",
+    chatPreview: "I'm a dog with a book I used to have a book but I ate it...! I like eating Books!",
+    newMessages: 0
+  },
+  {
+    id: 11,
+    nickname: "Crokus",
+    chatPreview: "I'm a dog with a book I used to have a book but I ate it...! I like eating Books!",
+    newMessages: 0
+  },
+]
+
 
 export default function TabHomiesScreen() {
+
+  const renderItem = ({item}) => {
+    return (
+      <View style={styles.item}>
+        <Text style={styles.nickname}>{item.nickname}</Text>
+        <Text style={styles.chatPreview}>{item.chatPreview}</Text>
+      </View>
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Homies</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabHomiesScreen.tsx" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={DATA}
+        renderItem={renderItem}
+      />
+    </SafeAreaView>
   );
 }
 
@@ -19,14 +108,24 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    width: '100%'
   },
-  title: {
+  nickname: {
     fontSize: 20,
     fontWeight: 'bold',
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  item: {
+    padding: 20,
+    marginVertical: 0,
+    marginHorizontal: 0,
+    borderRadius: 8,
+    borderColor: '#333333',
+    borderStyle: 'solid',
+    borderWidth: 1,
+    width: '100%'
   },
+  chatPreview: {
+    fontSize: 10,
+    overflow: 'visible',
+  }
 });
