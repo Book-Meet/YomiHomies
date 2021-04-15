6b0eca04ce57a413c194c36bb3f7a98798da55e2
MattB09 <mattbancroft03@gmail.com>
2021-04-15 18:39:44 +0900

    Cleanup.

--- screens/TabHomiesScreen.tsx
+++ screens/TabHomiesScreen.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { StyleSheet, FlatList, SafeAreaView, Text, View } from 'react-native';
 import Navigation from '../navigation';
-
+import Colors from '../constants/Colors';
 import EditScreenInfo from '../components/EditScreenInfo';
 import { navItem } from '@aws-amplify/ui';
 
