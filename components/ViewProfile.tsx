4d71fed7219278ecd45fec3f96b2110248cb4792
Misako Kondoh <kondoh.misako@gmail.com>
2021-04-19 15:35:06 +0900

    Add styles

--- components/ViewProfile.tsx
+++ components/ViewProfile.tsx
@@ -9,14 +9,16 @@ export default function viewProfile({ setViewMode, styles }) {
   return (
     <ScrollView>
       <View style={styles.container}>
-        <Text style={styles.title}>Profile</Text>
+        <Text style={styles.title}>{state.user.usename}</Text>
         <Image style={styles.profilePic} source={require('../assets/images/arina-reading.jpeg')} />
 
 
-        <Text>Username: {state.user.username}</Text>
-        <Text>Nickname: {state.user.nickname}</Text>
-        <Text>Gender: {state.user.gender}</Text>
-        <Text>Top Books: </Text>
+        {/* <Text style={styles.text}>Username: {state.user.username}</Text> */}
+        <View style={[{flexDirection: 'row'}, {alignContent: 'space-around'}]}>
+          <Text style={styles.text}>Nickname: {state.user.nickname}</Text>
+          <Text style={styles.text}>Gender: {state.user.gender}</Text>
+        </View>
+        <Text style={[styles.text, {justifyContent:'flex-start'}]}>Top Books: </Text>
             { state.user.books !== undefined ? state.user.books.items.map(book => {
             return (
               <Text key={book.id}>{book.title} - {book.author}</Text>
@@ -25,7 +27,7 @@ export default function viewProfile({ setViewMode, styles }) {
             : null
             }
         
-        <Text>Top Authors: </Text>
+        <Text style={[styles.text, {justifyContent:'flex-start'}]}>Top Authors: </Text>
             { state.user.authors !== undefined ? state.user.authors.items.map(auth => {
             return (
               <Text key={auth.id}>{auth.name}</Text>
@@ -33,8 +35,8 @@ export default function viewProfile({ setViewMode, styles }) {
             })
             : null
             }
-        <Text>About me: </Text>
-        <Text>{state.user.about_me}</Text>
+        <Text style={[styles.text, {justifyContent:'flex-start'}]}>About me: </Text>
+        <Text style={styles.text}>{state.user.about_me}</Text>
         <Pressable
           style={[styles.button, styles.buttonOpen]}
           onPress={() => setViewMode("edit")}
@@ -46,7 +48,10 @@ export default function viewProfile({ setViewMode, styles }) {
   );
 }
 
-// const styles = StyleSheet.create({
+const styles = StyleSheet.create({
+  subTitle: {
+    alignSelf: 'flex-start',
+  },
 //   container: {
 //     alignItems: 'center',
 //     justifyContent: 'center',
@@ -111,4 +116,4 @@ export default function viewProfile({ setViewMode, styles }) {
 //     marginBottom: 15,
 //     textAlign: 'center'
 //   }
-// });
+});
--- screens/TabProfileScreen.tsx
+++ screens/TabProfileScreen.tsx
@@ -68,6 +68,7 @@ export default function TabProfileScreen() {
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
     height: "100%"
@@ -80,6 +81,7 @@ const styles = StyleSheet.create({
     width: 100,
     height: 100,
     borderRadius: 10,
+    margin: 10,
   },
   separator: {
     marginVertical: 30,
@@ -131,7 +133,13 @@ const styles = StyleSheet.create({
   modalText: {
     marginBottom: 15,
     textAlign: 'center'
+  },
+  text: {
+    lineHeight: 20,
+    fontSize: 14,
+    margin: 5,
   }
+
 });
 
       {/* <Text>Top Genres: </Text>
