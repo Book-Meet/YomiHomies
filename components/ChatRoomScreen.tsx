679b1bba5bbd01207690642011d0d34e9006e12a
BBennettChiba <BrysonCBennett@gmail.com>
2021-04-27 14:41:48 +0900

    finally got live working

--- components/ChatRoomScreen.tsx
+++ components/ChatRoomScreen.tsx
@@ -8,28 +8,16 @@ import API, {graphqlOperation} from '@aws-amplify/api'
 //import { useRoute } from '@react-navigation/native';
 import ChatMessage from './ChatRoomContent';
 
-export default function ChatRoomScreen({myID, currentChat, setCurrentChat, matches, setMatches}) {
-    const [messages, setMessages] = useState(currentChat.chatRoomID.messages.items);
-    useEffect(()=>{
-        const subscription = API.graphql( {query:onCreateMessage, variables:{chatRoomID:currentChat.chatRoomID.id}}).subscribe({
-            next:(data) =>{
-                let localMatchesCopy = matches;
-                data = data.value.data.onCreateMessage
-                if(messages.length > 0 && messages[messages.length - 1].id === data.id) return;
-                localMatchesCopy[currentChat.index].chatRoomID.messages.items.push(data);
-                setMatches(localMatchesCopy);
-                setMessages(localMatchesCopy[currentChat.index].chatRoomID.messages.items);
-            }
-        })
-    },[])
+export default function ChatRoomScreen({myID, currentChat, setCurrentChat, chatRooms, setChatRooms}) {
 
     return (
         <ImageBackground style={{width: '100%', height: '100%'}} source={require('../assets/images/BG.png')}>
             <FlatList
-                data = {messages}
-                renderItem={({item}) => <ChatMessage myID={myID} message={item} />}
+                extraData={chatRooms}
+                data = {currentChat.messages.items}
+                renderItem={({item}) => <ChatMessage myID={myID} message={item} currentChat={currentChat}/>}
             />
-            <InputBox chatRoomID={currentChat.chatRoomID.id}myID={myID}/>
+            <InputBox chatRoomID={currentChat.id} myID={myID}/>
             <Button title='Leave' onPress={()=>{setCurrentChat(null)}}/>
         </ImageBackground>
     );
